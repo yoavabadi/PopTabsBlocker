3c8513ecb839775209fb0b6257c775588588891b
Yoav Abadi <yoavabadi@gmail.com>
2019-03-24 01:15:59 +0200

    Delete content_script.js

--- content_script.js
+++
@@ -1,5 +0,0 @@
-// In Next release - add changed url feature, via sendMeesage / check storage
-
-// window.addEventListener("hashchange", function (event) {
-//     alert("old url: " + event.oldURL.toString() + ", new url: " + event.newURL.toString());
-// });
